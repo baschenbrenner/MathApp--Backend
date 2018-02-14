b26d527f1766b11634b2749508fda7e24013b0f2
baschenbrenner <baschenbrenner@hotmail.com>
2018-02-13 19:50:00 -0600

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    resources :users, only: [:index, :show, :create] do
+      resources :games, only: [:index, :show, :create, :destroy]
+    end
+    resources :sessions, only: [:new, :create, :destroy]
+  end
 end
